f18c5d72df257ae047fde00e9afb0c02e0c0d458
jefmoura <jefmoura@protonmail.com>
2021-01-22 18:39:37 +0100

    Remove unnecessary files

--- app/db/base.py
+++
@@ -1,4 +0,0 @@
-# Import all the models, so that Base has them before being
-# imported by Alembic
-from app.db.base_class import Base  # noqa
-from app.review.models import Review, ReviewAnswer, ReviewEvent  # noqa
